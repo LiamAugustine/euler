56c76c592dbc7cb82eb1daba91de8f181e9a8008
LiamAugustine <LiamAugustine@foo>
2012-09-03 23:19:23 -0400

    G++ version of project1 from projecteuler.net

---
+++ project001.cpp
@@ -0,0 +1,29 @@
+/*Project 001 from projecteuler.net
+ *Find the sum of all the multiples of 3 or 5 below 1000
+ *Coded by: James Standeven
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+#define TOP 1000
+
+int main()
+{
+  int sum = 0;
+  
+  for(int c = 0; c < TOP; c++)
+  {
+    if(c % 3 == 0)
+      sum += c;
+    if(c % 5 == 0)
+      sum += c;
+  }
+
+  cout << sum << endl;
+
+  return 0;
+}
+
