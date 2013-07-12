c9ac5eb98185a13b3cc6d56aeae3ab4818787d42
LiamAugustine <LiamAugustine@foo>
2013-07-13 00:26:22 +0900

    Projecteuler.net solution #9

---
+++ project009.cpp
@@ -0,0 +1,27 @@
+/*
+	Projecteuler.net question 9
+	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+	Find the product abc.
+	
+	by James Standeven
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int m = 1,
+			n = 2,
+			a = 0,
+			b = 0,
+			c = 0;
+	while((a + b + c) != 1000){
+		a = (n*n) - (m*m);
+		b = 2 * n * m;
+		c = (n*n) + (m*m);
+		
+		cout << a*b*c << endl;
+		m++;
+		if(m == n) n++;
+	}
+}
